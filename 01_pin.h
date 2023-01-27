d8640b4c443714be8b90db801e88ad68a4e6a31d
noughtnaut <noughtnaut@golfbravo.net>
2023-01-27 14:08:36 +0100

    Make Pins class prettier, disable constructor chaining (cause of weird errors?)

--- 01_pin.h
+++ 01_pin.h
@@ -8,27 +8,39 @@ private:
   int number;     // Pin number on the Teensy board, as passed to `pinMode()`
   int mode;       // Pin input/output mode, as passed to `pinMode()`
   int actState;   // Pass (HIGH|LOW) to specify active signal level
-  int initState;  // Pass (HIGH|LOW) to specify initial signal level
 
 public:
 
-  Pin() {
-    Pin(0, OUTPUT, HIGH, LOW);
-  }
+//  Pin() {
+//    Serial.println("Pin()");
+//    Pin(99, INPUT, HIGH, false);
+//  }
+//
+//  Pin(int numberThis) {
+//    Serial.println("Pin(1)");
+//    Pin(numberThis, INPUT, HIGH, false);
+//  }
+//
+//  Pin(int numberThis, int modeThis, int actStateThis) {
+//    Serial.println("Pin(1,2,3)");
+//    Pin(numberThis, modeThis, actStateThis, false);
+//  }
 
-  Pin(int numberThis, int actStateThis) {
-    Pin(number, INPUT_PULLUP, actStateThis, !actStateThis);
-  }
-
-  Pin(int numberThis, int modeThis, int actStateThis, int initStateThis) {
+  Pin(int numberThis, int modeThis, int actStateThis, bool initiallyActive) {
+    String modeStr = String(
+      modeThis==INPUT?"INPUT"
+      :modeThis==OUTPUT?"OUTPUT"
+      :modeThis==INPUT_PULLUP?"INPUT_PULLUP"
+      :String(modeThis).c_str()
+    );
+//    Serial.printf("New Pin: %i, %s, %s, %s\n", numberThis, modeStr.c_str(), actStateThis==LOW?"LOW":"HIGH", initiallyActive?"on":"off");
     number = numberThis;
     mode = modeThis;
     actState = actStateThis;
-    initState = initStateThis;
 
     pinMode(number, mode);
     if (mode == OUTPUT) {
-      setState(initState);
+      setActive(initiallyActive);
     }
   }
 
@@ -39,15 +51,7 @@ public:
 
   void setActive(bool activate) {
     //Serial.printf("setActive(%i,%s) -> ", number , activate?"true":"false");
-    if (LOW == actState)
-      if (activate)
-        setState(LOW);
-      else
-        setState(HIGH);
-    else if (activate)
-      setState(HIGH);
-    else
-      setState(LOW);
+    setState(activate == (HIGH==actState)); // XNOR
   }
 
   bool isActive() {
