0b2b43395a482badf235b5ea5b58cb1b001c27f3
noughtnaut <noughtnaut@golfbravo.net>
2025-02-07 09:49:02 +0100

    Clean up logger implementation.

--- 01_logger.h
+++ 01_logger.h
@@ -15,23 +15,30 @@ private:
 
 	int indent = 0;
 	bool isEnabled = false;
+  char buffer[12]; // for integer-to-string conversion
 
-	void startSerial() {
+	void initSerial() {
 		pinLed.on();
 		Serial.begin(0);  // Will always be USB speed
 		pinLed.blink(50,50);
+    Serial.print("[Logger initialised]\n");
 	}
 
-	void logAtIndent(const char* msg, int _indent) {
+	void logAtIndent(const char* msg, int indent) {
 		if (!isEnabled) return;
-		for (int i=0; i<_indent; i++) Serial.print("  ");
+		for (int i=0; i<indent; i++) Serial.print("  ");
 		Serial.print(msg);
+	}
+
+	const char* iToStr(int i) {
+    sprintf(buffer, "%d", i);
+    return buffer;
 	}
 
 public:
 
 	Logger() {
-		startSerial();
+		initSerial();
 	}
 
 	void enable() {
@@ -42,37 +49,7 @@ public:
 		isEnabled = false;
 	}
 
-	void logln() {
-		log("\n");
-	}
-
-	void logln(char msg) {
-		char arr[1] = {msg};
-		logln(arr);
-	}
-
-	void logln(const char* msg) {
-		log(msg);
-		logln();
-	}
-
-	void log(String msg) {
-		log(msg.c_str());
-	}
-
-	void log(const char* msg) {
-		logAtIndent(msg, indent);
-	}
-
-	void appendln(const char* msg) {
-		append(msg);
-		logln();
-	}
-
-	void append(const char* msg) {
-		logAtIndent(msg, 0);
-	}
-
+  //Managing blocks (indentation)
 	void begin(const char* msg) {
 		logln(msg);
 		indent++;
@@ -83,8 +60,46 @@ public:
 	}
 
 	void end(const char* msg) {
-		indent--;
+		if (indent>0) indent--;
 		logln(msg);
+	}
+
+  //Logging (with block indentation, and line feed)
+	void logln(int i) {
+    logln(iToStr(i));
+	}
+
+	void logln(const char* msg) {
+		logAtIndent(msg, indent);
+		append("\n");
+	}
+
+  //Logging (with block indentation, no line feed for continuation)
+	void log(int i) {
+    log(iToStr(i));
+	}
+
+	void log(const char* msg) {
+		logAtIndent(msg, indent);
+	}
+
+  // Appending (without block indentation, and line feed)
+	void appendln(int i) {
+    appendln(iToStr(i));
+	}
+
+	void appendln(const char* msg) {
+		logAtIndent(msg, 0);
+		append("\n");
+	}
+
+  // Appending (without block indentation, no line feed for continuation)
+	void append(int i) {
+    append(iToStr(i));
+	}
+
+	void append(const char* msg) {
+		logAtIndent(msg, 0);
 	}
 };
 
